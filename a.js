d8deb6c364a5e939c6f113a46f97f65456149d53
ajay <okab2028@gmail.com>
2023-04-03 12:49:36 +0530

    a

---
+++ a.js
@@ -0,0 +1,16 @@
+
+
+ const AddTwoNumbers =(a,b)=>
+ {
+    
+     return a+b;
+    
+ };
+ AddTwoNumbers();
+
+
+//  const AddTwoNumbersa =(a,b)=>
+//  {
+//      console.log(a+b);
+//  }
+//  AddTwoNumbersa(2,3);
